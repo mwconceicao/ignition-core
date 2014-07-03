b16805ed7328837864150c6598b3cce4a290ac7d
Allan Douglas R. de Oliveira <allan@chaordicsystems.com>
2014-07-02 23:00:05 -0300

    Moving JobSetup to core

---
+++ src/main/scala/ignition/core/jobs/JobSetup.scala
@@ -0,0 +1,22 @@
+package ignition.core.jobs
+
+import scala.util.Try
+
+trait JobSetup {
+
+  def executeRetrying[T](code: => T, maxExecutions: Int = 3): T = {
+    assert(maxExecutions > 0) // we will execute at least once
+    // TODO: log retries
+
+    def _executeRetrying(retriesLeft: Int): Try[T] = {
+      val tryResult = Try { code }
+      if (tryResult.isFailure && retriesLeft > 0) {
+        _executeRetrying(retriesLeft - 1)
+      } else {
+        tryResult
+      }
+    }
+
+    _executeRetrying(maxExecutions - 1).get
+  }
+}
