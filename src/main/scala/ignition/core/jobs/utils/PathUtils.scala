458cfb891d3a4f683bb404269a753aa5e6455ffc
Allan Douglas R. de Oliveira <allan@chaordicsystems.com>
2014-09-07 14:17:34 -0300

    Fixed date handling for EP format

--- src/main/scala/ignition/core/jobs/utils/PathUtils.scala
+++ src/main/scala/ignition/core/jobs/utils/PathUtils.scala
@@ -1,17 +1,17 @@
 package ignition.core.jobs.utils
 
-import org.joda.time.DateTime
+import org.joda.time.{DateTimeZone, DateTime}
 import org.joda.time.format.DateTimeFormat
 
 import scala.util.Try
 
 object PathUtils {
 
-  private val datePatterns = Seq(DateTimeFormat.forPattern("'dt='yyyy-MM-dd"), // mail-dumper format
-    DateTimeFormat.forPattern("yyyy-MM-dd"), // platform format
-    DateTimeFormat.forPattern("yyyyMMdd-HHmmss"), // engine format
-    DateTimeFormat.forPattern("yyyyMMdd_HHmmss"), // EP format
-    DateTimeFormat.forPattern("yyyy_MM_dd'T'HH_mm_ss'UTC'")) // ignition format
+  private val datePatterns = Seq(DateTimeFormat.forPattern("'dt='yyyy-MM-dd").withZoneUTC(), // mail-dumper format
+    DateTimeFormat.forPattern("yyyy-MM-dd").withZoneUTC(), // platform format
+    DateTimeFormat.forPattern("yyyyMMdd-HHmmss").withZoneUTC(), // engine format
+    DateTimeFormat.forPattern("yyyyMMdd_HHmmss").withZone(DateTimeZone.forID("America/Sao_Paulo")), // EP format
+    DateTimeFormat.forPattern("yyyy_MM_dd'T'HH_mm_ss'UTC'").withZoneUTC()) // ignition format
 
   def extractDate(path: String): DateTime = {
     val segments = path.split("/")
