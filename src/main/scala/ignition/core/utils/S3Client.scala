c67b5fb807d8d0bb687c83ce45bbdecb462bb222
ZaGo <zago@chaordic.com.br>
2014-09-11 16:02:33 -0300

    Consider default to S3Client

--- src/main/scala/ignition/core/utils/S3Client.scala
+++ src/main/scala/ignition/core/utils/S3Client.scala
@@ -2,10 +2,10 @@ package ignition.core.utils
 
 import java.util.Properties
 
-import org.jets3t.service.{Constants, Jets3tProperties}
 import org.jets3t.service.impl.rest.httpclient.RestS3Service
 import org.jets3t.service.model.{S3Bucket, S3Object}
 import org.jets3t.service.security.AWSCredentials
+import org.jets3t.service.{Constants, Jets3tProperties}
 
 
 class S3Client {
@@ -13,8 +13,9 @@ class S3Client {
   val jets3tProperties = {
     val jets3tProperties = Jets3tProperties.getInstance(Constants.JETS3T_PROPERTIES_FILENAME);
     val properties = new Properties()
-    properties.put("httpclient.max-connections", "50") // The maximum number of simultaneous connections to allow globally
-    properties.put("httpclient.retry-max", "20") // How many times to retry connections when they fail with IO errors
+//    properties.put("httpclient.max-connections", "2") // The maximum number of simultaneous connections to allow globally
+//    properties.put("httpclient.retry-max", "10") // How many times to retry connections when they fail with IO errors
+//    properties.put("httpclient.socket-timeout-ms", "30000") // How many milliseconds to wait before a connection times out. 0 means infinity.
 
     jets3tProperties.loadAndReplaceProperties(properties, "chaordic'")
     jets3tProperties
