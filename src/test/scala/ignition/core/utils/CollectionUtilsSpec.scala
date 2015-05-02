2558863acebf8c518d14203821bd4c198bdb659b
Allan Oliveira <allan@chaordicsystems.com>
2015-05-02 12:12:37 -0300

    Added some tests

---
+++ src/test/scala/ignition/core/utils/CollectionUtilsSpec.scala
@@ -0,0 +1,26 @@
+package ignition.core.utils
+
+import org.scalatest._
+import CollectionUtils._
+
+class CollectionUtilsSpec extends FlatSpec with ShouldMatchers {
+
+  case class MyObj(property: String, value: String)
+  "CollectionUtils" should "provide distinctBy" in {
+    val list = List(MyObj("p1", "v1"), MyObj("p2", "v1"), MyObj("p1", "v2"), MyObj("p2", "v2"))
+    list.distinctBy(_.property) shouldBe List(MyObj("p1", "v1"), MyObj("p2", "v1"))
+    list.distinctBy(_.value) shouldBe List(MyObj("p1", "v1"), MyObj("p1", "v2"))
+  }
+
+  it should "provide compress" in {
+    List("a", "a", "b", "c", "e", "e", "c", "d", "e").compress shouldBe List("a", "b", "c", "e", "c", "d", "e")
+  }
+
+  it should "provide compressBy" in {
+    val list = List(MyObj("p1", "v1"), MyObj("p2", "v1"), MyObj("p1", "v2"), MyObj("p2", "v2"))
+    list.compressBy(_.property) shouldBe list
+    list.compressBy(_.value) shouldBe List(MyObj("p1", "v1"), MyObj("p1", "v2"))
+  }
+
+
+}
