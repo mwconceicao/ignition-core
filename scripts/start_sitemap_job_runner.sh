24063f3221a65c95f15525ecb2139ed3d0f6f261
Allan Oliveira <allan@chaordicsystems.com>
2015-06-12 20:28:32 -0300

    Small improvement

--- scripts/start_sitemap_job_runner.sh
+++ scripts/start_sitemap_job_runner.sh
@@ -2,11 +2,12 @@
 
 
 IGNITION_HOME=/home/search/search-ignition
-RESULTS_DIR=/var/log/search-ignition
+LOG_DIR=/var/log/search-ignition
+RESULTS_DIR=$LOG_DIR/results
 
 mkdir -p $RESULTS_DIR
 export LANG=C
 export LC_ALL=C
 
-nohup $IGNITION_HOME/scripts/job_runner.py sitemap-generation $RESULTS_DIR >& $IGNITION_HOME/sitemap_job_runner.log &
+nohup $IGNITION_HOME/scripts/job_runner.py sitemap-generation $RESULTS_DIR >& $LOG_DIR/sitemap_job_runner.log &
 
