8f442c009226f2249310e9672e4471683edd5860
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2015-06-30 18:00:44 -0300

    Skeleton

--- src/main/scala/ignition/jobs/utils/DashboardAPI.scala
+++ src/main/scala/ignition/jobs/utils/DashboardAPI.scala
@@ -1,8 +1,28 @@
 package ignition.jobs.utils
 
-/**
- * Created by flavio on 6/30/15.
- */
+import ignition.jobs.ResultPoint
+import spray.client.pipelining.{SendReceive, _}
+import spray.http.BasicHttpCredentials
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.{implicitConversions, postfixOps}
+
+
 object DashboardAPI {
 
+  val dashboardId = ""
+  val passwd = ""
+
+  val baseHref = "https://dashboard-api-test.chaordicsystems.com/v2/kpi"
+
+  //search/kpi_test
+  def dailyFact(product: String, kpi: String, resultPoint: ResultPoint) = {
+    dashboardPipeline(Post(s"$baseHref/$product/$kpi", resultPoint))
+  }
+
+  val dashboardPipeline: SendReceive = (
+    addCredentials(BasicHttpCredentials(dashboardId, passwd))
+      ~> sendReceive
+    )
 }
+
