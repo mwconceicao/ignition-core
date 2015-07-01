0d3a1a33e547c6949fcb17a0212a43f9597a7497
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2015-07-01 10:58:32 -0300

    add some funcionality

--- src/main/scala/ignition/jobs/utils/DashboardAPI.scala
+++ src/main/scala/ignition/jobs/utils/DashboardAPI.scala
@@ -2,10 +2,13 @@ package ignition.jobs.utils
 
 import akka.actor.ActorSystem
 import ignition.jobs.ResultPoint
+import org.joda.time.Interval
 import spray.client.pipelining.{SendReceive, _}
-import spray.http.BasicHttpCredentials
+import spray.http._
+import spray.http.ContentTypes.`application/json`
 import spray.httpx.SprayJsonSupport._
 
+import scala.concurrent.Future
 import scala.language.{implicitConversions, postfixOps}
 
 object DashboardAPI {
@@ -21,14 +24,41 @@ object DashboardAPI {
   val baseHref = "https://dashboard-api-test.chaordicsystems.com/v2/kpi"
 
   //search/kpi_test
-  def dailyFact(product: String, kpi: String, resultPoint: ResultPoint) = {
+    /**
+     * Send a Daily Fact to DashboardAPI
+     *
+     * @param product Product identification on dashboard
+     * @param kpi KPI to send metric
+     * @param resultPoint Metric to send
+     *
+     * @return a future with the result of this operation.
+     */
+  def dailyFact(product: String, kpi: String, resultPoint: ResultPoint): Future[HttpResponse] = {
     // TODO: need to change key to day or create a case class for sending it
     dashboardPipeline(Post(s"$baseHref/$product/$kpi", resultPoint))
   }
 
+  /**
+   * Delete multiple metrics from DashboardAPI
+   *
+   * @param product Product identification on dashboard
+   * @param kpi KPI to delete metric
+   * @param clients Set of clients
+   * @param interval Interval to delete
+   * @return a future with the result of this operation
+   */
+
+  def deleteDailyFact(product: String, kpi: String, clients: Set[String],
+                       interval: Interval): Future[HttpResponse] = {
+    dashboardPipeline(
+      Delete(s"""$baseHref/$product/$kpi?from=${interval.getStart}&to=${interval.getEnd}&client=${clients.mkString(",")}"""))
+  }
+
   val dashboardPipeline: SendReceive = (
+    addHeader(HttpHeaders.`Content-Type`(`application/json`)) ~>
     addCredentials(BasicHttpCredentials(dashboardId, passwd))
       ~> sendReceive
     )
+
 }
 
