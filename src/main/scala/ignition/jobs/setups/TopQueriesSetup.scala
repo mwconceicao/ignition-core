916520254084f0981507ef4e457887181e857cfc
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2015-07-09 10:04:59 -0300

    Change S3 output

--- src/main/scala/ignition/jobs/setups/TopQueriesSetup.scala
+++ src/main/scala/ignition/jobs/setups/TopQueriesSetup.scala
@@ -22,7 +22,7 @@ object TopQueriesSetup extends SearchETL  {
     TopQueriesJob.execute(parsedSearchLogs)
       .repartition(numPartitions = 1)
       .map(transformToJsonString)
-      .saveAsTextFile(s"s3://chaordic-search-ignition-latest/top-queries/${config.tag}")
+      .saveAsTextFile(s"s3://chaordic-search-ignition-history/top-queries/${config.tag}")
 
     logger.info(s"TopQueriesJob done.")
   }
