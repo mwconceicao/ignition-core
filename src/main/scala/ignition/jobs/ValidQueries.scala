876d97dd0bfa26dcc4b266d0003e297e475d59e2
Fernando Luiz Parisotto <fernando.parisotto@chaordicsystems.com>
2015-07-10 10:13:51 -0300

    wip valid queries

--- src/main/scala/ignition/jobs/ValidQueries.scala
+++ src/main/scala/ignition/jobs/ValidQueries.scala
@@ -3,6 +3,7 @@ package ignition.jobs
 import ignition.chaordic.pojo.{SearchEvent, SearchClickLog, SearchLog}
 import org.apache.spark.rdd.RDD
 import org.joda.time.DateTime
+import ignition.jobs.utils.text.{Tokenizer, ChaordicStemmer}
 
 object ValidQueries {
 
@@ -77,10 +78,86 @@ object ValidQueries {
    * @param query Query to be normalized.
    * @return
    */
-  def normalize_query(query: String) =
+  def normalizeQuery(query: String) =
     query.trim.toLowerCase.removeTyposAtQueryTail()
 
-  def process(searchLogs: RDD[SearchLog], clickLogs: RDD[SearchClickLog]) = {
+  def queryToKey(query: String) =
+    ChaordicStemmer.asciiFold(query.split("\\s+")).mkString
+
+  def extractTokens(query: String): Seq[String] =
+    ChaordicStemmer.stem(ChaordicStemmer.asciiFold(Tokenizer.tokenize(query)).map(_.toLowerCase))
+
+  def joinSearchLogsWithClickLogs(validSearchLogs: RDD[((String, String), SearchLog)],
+                                  validClickLogs: RDD[((String, String), SearchClickLog)]) = {
+
+    val filteredClickLogs: RDD[((String, String), SearchClickLog)] =
+      validSearchLogs
+        .map { case ((apiKey, searchId), log) => ((apiKey, searchId), log.query) }
+        .distinct()
+        .join(validClickLogs)
+        .filter { case ((apiKey, searchId), (query, log)) => query.nonEmpty }
+        .map { case ((apiKey, searchId), (query, clickLog)) => ((apiKey, query), clickLog) }
+
+
+    val filteredSearchLogs: RDD[((String, String), SearchLog)] =
+      validSearchLogs
+        .map { case ((apiKey, searchId), log) => ((apiKey, log.query), log) }
+
+    val clicks: RDD[((String, String), Long)] = filteredClickLogs.aggregateByKey(0L)((_, _) => 1, _ + _)
+    val searches: RDD[((String, String), Long)] = filteredSearchLogs.aggregateByKey(0L)((_, _) => 1, _ + _)
+
+    val sumOfResults = filteredSearchLogs.mapValues(_.totalFound.toLong).reduceByKey(_ + _)
+
+    val latestSearchLogs = filteredSearchLogs.reduceByKey((s1, s2) => if (s1.date.isAfter(s2.date)) s1 else s2)
+
+    val validQueries = clicks.join(searches).join(latestSearchLogs).join(sumOfResults).map {
+      case ((apiKey, query), (((clickCount, searchCount), latestSearchLog), sumResult)) =>
+        ValidQuery(apiKey = apiKey,
+          query = query,
+          searches = searchCount,
+          clicks = clickCount,
+          rawCtr = clickCount.toDouble / searchCount,
+          latestSearchLog = latestSearchLog.date,
+          latestSearchLogResults = latestSearchLog.totalFound,
+          latestSearchLogFeature = latestSearchLog.feature,
+          sumResults = sumResult,
+          averageResults = sumResult / searchCount)
+    }
+
+    val biggestValidQuery = validQueries
+      .keyBy(validQuery => (validQuery.apiKey, queryToKey(validQuery.query)))
+      .reduceByKey((v1, v2) => if (v1.searches > v2.searches) v1 else v2)
+      .map {
+      case ((apiKey, _), validQuery) =>
+        val tokens = extractTokens(validQuery.query)
+        ((apiKey, tokens), validQuery)
+    }
+
+    biggestValidQuery.groupByKey().map {
+      case ((apiKey, tokens), allValidQueries) =>
+        val queries = allValidQueries.toSeq
+        val topValidQuery = queries.toSeq.sortBy(_.searches).reverse.head
+        val latestValidQuery = queries.toSeq.sortBy(_.latestSearchLog).reverse.head
+        val totalSearches = queries.map(_.searches).sum
+        val totalClicks = queries.map(_.clicks).sum
+        val totalResult = queries.map(_.sumResults).sum
+        ValidQueryFinal(
+          apiKey = apiKey,
+          tokens = tokens,
+          topQuery = topValidQuery.query,
+          searches = totalSearches,
+          clicks = totalClicks,
+          rawCtr = totalClicks / totalSearches,
+          latestSearchLog = latestValidQuery.latestSearchLog,
+          latestSearchLogResults = latestValidQuery.latestSearchLogResults,
+          averageResults = totalResult / totalSearches,
+          queries = queries
+        )
+    }
+
+  }
+
+  def process(searchLogs: RDD[SearchLog], clickLogs: RDD[SearchClickLog]): RDD[ValidQueryFinal] = {
 
     val validSearchLogs = getValidSearchLogs(searchLogs)
       .keyBy(searchLog => (searchLog.apiKey, searchLog.searchId))
@@ -88,19 +165,31 @@ object ValidQueries {
     val validClickLogs = getValidClickLogs(clickLogs)
       .keyBy(clickLog => (clickLog.apiKey, clickLog.searchId))
 
-
+    joinSearchLogsWithClickLogs(validSearchLogs, validClickLogs)
 
   }
 
   case class ValidQuery(apiKey: String,
                         query: String,
-                        searches: Int,
-                        clicks: Int,
-                        raw_ctr: Double,
+                        searches: Long,
+                        clicks: Long,
+                        rawCtr: Double,
                         latestSearchLog: DateTime,
-                        latestSearchLogResults: DateTime,
-                        latestSearchLogFeature: DateTime,
-                        sumResults: Int,
-                        averageResults: Int)
+                        latestSearchLogResults: Int,
+                        latestSearchLogFeature: String,
+                        sumResults: Long,
+                        averageResults: Long)
+
+  case class ValidQueryFinal(apiKey: String,
+                             tokens: Seq[String],
+                             topQuery: String,
+                             searches: Long,
+                             clicks: Long,
+                             rawCtr: Double,
+                             latestSearchLog: DateTime,
+                             latestSearchLogResults: Int,
+                             averageResults: Long,
+                             queries: Seq[ValidQuery],
+                             active: Boolean = true)
 
 }
