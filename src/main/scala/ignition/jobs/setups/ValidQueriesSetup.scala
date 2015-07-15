a3e4a558cdf04e2fdff71ab7c2a2ec6567612304
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2015-07-15 11:18:13 -0300

    Use 6 months

--- src/main/scala/ignition/jobs/setups/ValidQueriesSetup.scala
+++ src/main/scala/ignition/jobs/setups/ValidQueriesSetup.scala
@@ -4,6 +4,7 @@ import ignition.chaordic.utils.Json
 import ignition.core.jobs.CoreJobRunner.RunnerContext
 import ignition.jobs.ValidQueriesJob.ValidQueryFinal
 import ignition.jobs.{SearchETL, ValidQueriesJob}
+import org.apache.spark.storage.StorageLevel
 import org.slf4j.{Logger, LoggerFactory}
 
 
@@ -17,16 +18,17 @@ object ValidQueriesSetup extends SearchETL {
     val config = runnerContext.config
     val now = runnerContext.config.date
 
-    val start = parseDateOrElse(config.additionalArgs.get("start"), config.date.minusDays(356).withTime(23, 59, 59, 999))
+    val start = parseDateOrElse(config.additionalArgs.get("start"), config.date.minusDays(180).withTime(23, 59, 59, 999))
     val end = parseDateOrElse(config.additionalArgs.get("end"), config.date.minusDays(1).withTimeAtStartOfDay())
 
     logger.info(s"Starting ValidQueries for start=$start, end=$end")
 
-    val parsedSearchLogs = parseSearchLogs(config.setupName, sc, start = start, end = now)
-    val parsedClickLogs = parseClickLogs(config.setupName, sc, start = start, end = now)
+    val parsedSearchLogs = parseSearchLogs(config.setupName, sc, start = start, end = now).persist(StorageLevel.MEMORY_AND_DISK)
+    val parsedClickLogs = parseClickLogs(config.setupName, sc, start = start, end = now).persist(StorageLevel.MEMORY_AND_DISK)
 
     ValidQueriesJob.process(parsedSearchLogs, parsedClickLogs)
       .map(transformToJsonString)
+      .coalesce(20)
       .saveAsTextFile(s"s3n://chaordic-search-ignition-history/valid-queries/${config.tag}")
   }
 
