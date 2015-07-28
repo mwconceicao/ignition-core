d5a301ef83a8d84ac59db393428386ef4f2d5ef9
Fernando Luiz Parisotto <fernando.parisotto@chaordicsystems.com>
2015-07-28 13:42:08 -0300

    force rebuild

--- scripts/start_searchetl_job_runner.sh
+++ scripts/start_searchetl_job_runner.sh
@@ -8,4 +8,6 @@ mkdir -p $RESULTS_DIR
 export LANG=C
 export LC_ALL=C
 
+#Force assembly rebuild
+rm -rf $IGNITION_HOME/target
 nohup $IGNITION_HOME/scripts/job_runner.py search-etl-generation $RESULTS_DIR >& $LOG_DIR/search-etl-generation_job_runner.log &
