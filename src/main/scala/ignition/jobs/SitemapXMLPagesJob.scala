2e8668f630a85d0fa8f9e524bed0cd9d7ad81fc0
Larissa Lautert <larissalautert@gmail.com>
2015-10-20 15:29:07 -0200

    sitemap: queries frequency changed from 'daily' to 'weekly'

--- src/main/scala/ignition/jobs/SitemapXMLPagesJob.scala
+++ src/main/scala/ignition/jobs/SitemapXMLPagesJob.scala
@@ -97,7 +97,7 @@ object SitemapXMLSearchJob {
     val queryStrings = combinedQueriesWithRanks.map {
       query =>
         val link = s"${config.normalizedHost}/?q=${encode(query)}"
-        generateUrlXml(link, now, "daily", 0.5)
+        generateUrlXml(link, now, "weekly", 0.5)
     }
 
     queryStrings
